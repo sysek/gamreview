6d93d3aebad822566ee7d9fc42a1b33738891829
sysek <mateusz.mielczarek2@gmail.com>
2019-03-21 06:09:22 +0100

    dodanie branchu dev

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Article
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "articles/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data()
+        context['articles'] = Article.objects.order_by('-date')
+        return context
