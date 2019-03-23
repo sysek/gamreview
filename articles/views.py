d20bf724bf2ae8c42dcd2798f87d186fccf6afad
sysek <mateusz.mielczarek2@gmail.com>
2019-03-23 14:29:10 +0100

    dodanie templates/articles/base.html

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Article
+
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "articles/base.html"
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data()
