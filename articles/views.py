b8945c92f07b06a9462f9363c6dc3e4584b310ec
sysek <mateusz.mielczarek2@gmail.com>
2019-03-29 13:49:45 +0100

    jakies bzdety

--- articles/views.py
+++ articles/views.py
@@ -17,6 +17,7 @@ class ArticleView(DetailView):
     model = Article
 
     def get_context_data(self, **kwargs):
-        context = super(ArticleView, self).get_context_data()
-        context['art'] = Article.objects.all()
+        context = super(ArticleView, self).get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
         return context
+
--- gamereview/urls.py
+++ gamereview/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view()),
-    path('article/<slug:slug>/', ArticleView.as_view(), name='article'),
-]
+    path('article/<slug:slug>', ArticleView.as_view(), name='article'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
