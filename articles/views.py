f9fe7f17f866467e0bd31dcff73ed43eccf2cee9
sysek <mateusz.mielczarek2@gmail.com>
2019-06-13 08:53:01 +0200

    zmiana wyswietlania base.html

--- articles/views.py
+++ articles/views.py
@@ -4,20 +4,11 @@ from .models import Article
 
 # Create your views here.
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = "articles/base.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data()
-        context['articles'] = Article.objects.all()
-        return context
-
-
-class ArticleListView(ListView):
-    model = Article
-    template_name = 'articles/article_list.html'
-    paginate_by = 5
     queryset = Article.objects.all()
+    paginate_by = 5
+
 
 class ArticleView(DetailView):
     model = Article
